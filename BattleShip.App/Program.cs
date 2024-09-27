3d871f1583f3498d2f9768713e78839eba50baf1
TheRealOof <grassetc31@gmail.com>
2024-09-27 22:40:24 +0200

    Begin OAuth

--- BattleShip.App/Program.cs
+++ BattleShip.App/Program.cs
@@ -8,4 +8,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// add google oauth
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("OpenIdConnect", options.ProviderOptions);
+});
+
+
 await builder.Build().RunAsync();

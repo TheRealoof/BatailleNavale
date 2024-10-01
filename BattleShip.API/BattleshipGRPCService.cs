1603f8a00cd35d92f88326dfbc648a6d0d1c7d58
TheRealOof <grassetc31@gmail.com>
2024-10-01 11:02:08 +0200

    GRPC !!!

--- BattleShip.API/BattleshipGRPCService.cs
+++ BattleShip.API/BattleshipGRPCService.cs
@@ -1,6 +1,19 @@
-﻿namespace BattleShip.API;
+﻿using Grpc.Core;
+
+namespace BattleShip.API;
 
 public class BattleshipGRPCService : BattleshipService.BattleshipServiceBase
 {
 
+    public readonly GameService GameService;
+    
+    public BattleshipGRPCService(GameService gameService)
+    {
+        this.GameService = gameService;
+    }
+    
+    public override Task<AttackResponseGRPC> Attack(AttackRequestGRPC request, ServerCallContext context)
+    {
+        return base.Attack(request, context);
+    }
 }

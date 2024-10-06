638bb2a26d8bb1b59782065838d08f6b81dae778
TheRealOof <grassetc31@gmail.com>
2024-10-06 23:19:01 +0200

    Route View ❓

---
+++ BattleShip.App/Attributes/RequireGameHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BattleShip.App.Attributes;
+
+public class RequireGameHub : AuthorizeAttribute
+{
+    
+}
---
+++ BattleShip.App/Components/CustomRouteView.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BattleShip.App.Components;
+
+public class CustomRouteView : RouteView
+{
+    protected override void Render(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder builder)
+    {
+        base.Render(builder);
+    }
+}
